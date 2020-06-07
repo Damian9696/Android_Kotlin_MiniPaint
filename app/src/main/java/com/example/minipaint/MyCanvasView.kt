64c69fb767c0d17c6d00ae8e0c3790cf5b316955
Damian <damian.ogorek.1996@gmail.com>
2020-06-07 21:26:29 +0200

    Override onSizeChanged().

--- app/src/main/java/com/example/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/example/minipaint/MyCanvasView.kt
@@ -1,8 +1,29 @@
 package com.example.minipaint
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
 
 class MyCanvasView(context: Context?) : View(context) {
 
+    //Caching what has been drawing before
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
+
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        //Leaving the old Bitmaps around produce memory leak, to fix this, recycle extrBitmap before
+        //creating the new one
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+
+        //Recommended Bitmap.Config.ARGB_8888
+        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+    }
 }
