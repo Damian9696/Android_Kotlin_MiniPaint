06592a4a4bba186994991fab4fb1a257df71faa1
Damian <damian.ogorek.1996@gmail.com>
2020-06-08 23:09:19 +0200

    Added drawing frames around canvas.

--- app/src/main/java/com/example/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/example/minipaint/MyCanvasView.kt
@@ -2,10 +2,7 @@ package com.example.minipaint
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewConfiguration
@@ -48,6 +45,8 @@ class MyCanvasView(context: Context?) : View(context) {
     //No need to draw every pixel, interpolate a path between points for much better performance
     private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
 
+    private lateinit var frame: Rect
+
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
 
@@ -59,11 +58,17 @@ class MyCanvasView(context: Context?) : View(context) {
         extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         extraCanvas = Canvas(extraBitmap)
         extraCanvas.drawColor(backgroundColor)
+
+        //Calculate a rectangular frame the picture
+        val inset = 40
+        frame = Rect(inset, inset, width - inset, height - inset)
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         canvas?.drawBitmap(extraBitmap, 0f, 0f, null)
+        //Draw a frame around the canvas
+        canvas?.drawRect(frame, paint)
     }
 
     private fun touchStart() {
